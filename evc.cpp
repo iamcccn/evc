b553a64b0a3f18179bedd1359990fc2bc2128b25
Abdul_Samad Ali <88903073+iamcccn@users.noreply.github.com>
2021-12-15 16:17:57 +0300

    Update and rename README.md to evc.cpp

---
+++ evc.cpp
@@ -0,0 +1,16 @@
+# evc
+
+#include<stdio.h>
+int main()
+{
+printf("Fadlan Gali Furaha -EVC-: \n");
+	scanf("\n*770#");
+	printf("-EVC-PlUS- \nFadlan Geli PIN-Kaaga(Enter pin) \n");
+	scanf("\n2003");
+	printf("  \n1.Itus Haraaga \n2.Kaarka Hadalka \n3.Bixi Biil \n4.U wareeji \n5.Warbixin Kooban \n6.Salaam Bank \n7.Maareyn \n8.Taaj \n");
+	scanf("  \n1");
+    printf("  \n[-EVCPlus-] Haraaga Xisaabtaadu Waa:$0@");
+    printf("  \n");
+    scanf("  \n12");
+    printf("     \n                              WAXA AAD GALISAY WAA QALAD!!!                 \n                                FADLAN RAAC TILMAAMAHAN:                          \n                       WAXAAD GALISA MARKA KOOWAAD *770#                                \n                               Kadib Waxaad Galisaa PInka \n                                    KADIB DOORO NUMBER 1. \n          . WAXA UUKUUSOOSARAYA JAWAB SAX AH QORAALKAN MA ARKI DOONTID QORAALKAN \n          ????????????????????????????????????????????????????????????????????????? \n                                     ...KULAABO...                                                                          \n                                ...MACASALAAMA...");
+}
